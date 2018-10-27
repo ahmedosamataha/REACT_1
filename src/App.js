c6bb5a4c40553c261fedf7a8e22133b663204049
Ahmed Osama <ahmedosama.t123@gmail.com>
2018-10-27 21:43:36 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+  constructor(){
+    super()
+    this.state={
+      pageOn : true,
+      colorAccess : false,
+      data : "  "
+    }
   }
+operation()
+{
+  this.setState({
+    pageOn : !this.state.pageOn
+  })
+}
+handle(event){
+
+    this.setState({
+      data:event.target.value
+    })
+}
+myColor(){
+  this.setState({
+    colorAccess : !this.state.colorAccess
+  })
 }
 
+  render(){
+    return(
+      <div>
+      {this.state.pageOn?
+      <div>
+        <h1> "Osama's first react app" </h1>
+
+        <textbox> color : <input type="text"
+         onChange={this.handle.bind(this)}/>
+         </textbox>
+         <button onClick={()=>this.myColor()}> SHOW ? </button>
+         <div >
+         {this.state.colorAccess?
+         <textarea style={{backgroundColor: this.state.data }}> please
+          type the color u want above </textarea>
+
+         :
+         <textarea style={{backgroundColor: 'white' }}> please
+          type the color u want above </textarea>
+       }
+         </div>
+
+        </div>
+        :null
+      }
+        <button onClick={()=>this.operation()}> SHOW CONTENT ? </button>
+      </div>
+    )
+  }
+}
 export default App;
